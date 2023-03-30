377c2b3bf502572090bc4cd8334894b639310e76
Lorrayne Reis <loohsilva995@gmail.com>
2023-03-30 16:42:19 -0300

    Path to txt file

---
+++ Development/treat_source_file.py
@@ -0,0 +1,12 @@
+import os
+location = os.getcwd() # This will return the Current Working Directory
+
+file_txt = "Source/logSupercomputador.txt" # Txt location
+file_path = location.replace("Development",file_txt) # Path to txt
+print(file_path)
+
+
+
+
+
+
